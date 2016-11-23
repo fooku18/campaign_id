e9176d72c5004617d0fa9e1595b2613cf53da305
fooku18 <jakob.schlonsok@yahoo.de>
2016-11-23 18:06:22 +0100

    update

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 var express = require("express");
+var mysql = require("mysql");
+var connection = mysql.createConnection({
+	host: "localhost",
+	user: "standard",
+	password: "horst",
+	database: "controlling"
+})
 var app = express();
 
 //express config
@@ -7,7 +14,10 @@ app.set("view engine","pug");
 app.set("views","./views");
 
 app.get("/",function(req,res,next) {
-	res.status(200).render("index");
+	connection.query("SELECT * FROM test;",function(err,rows,fields) {
+		if(err) throw err;
+		res.status(200).send(rows);
+	})
 })
 
 app.listen(1337,"0.0.0.0",function() {
